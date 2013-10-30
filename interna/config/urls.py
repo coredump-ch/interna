a93520b542d11ce272401b868630128b80a2b3a9
Danilo Bargen <gezuru@gmail.com>
2013-10-30 01:37:53 +0100

    Added robots.txt

--- interna/config/urls.py
+++ interna/config/urls.py
@@ -1,11 +1,15 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from django.http import HttpResponse
 
 admin.autodiscover()
+
+robots = lambda r: HttpResponse('User-agent: *\nDisallow: /', mimetype='text/plain')
 
 urlpatterns = patterns('',
     url(r'^', include('front.urls')),
     url(r'^', include('memberdb.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^robots\.txt$', robots)
 )
