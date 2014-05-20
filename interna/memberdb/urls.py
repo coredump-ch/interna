8109bb0b42d154e895c38847cad527c860112aa1
Danilo Bargen <mail@dbrgn.ch>
2014-05-20 18:28:15 +0200

    Dependeny updates

--- interna/memberdb/urls.py
+++ interna/memberdb/urls.py
@@ -2,7 +2,6 @@
 from __future__ import print_function, division, absolute_import, unicode_literals
 
 from django.conf.urls import patterns
-#from django.conf.urls import include, url
 
 
 urlpatterns = patterns('',)
