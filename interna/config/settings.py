350a53062c602fb76782bbd4630eb002523ea18f
Danilo Bargen <mail@dbrgn.ch>
2016-11-09 23:09:45 +0100

    Upgrade dependencies

--- interna/config/settings.py
+++ interna/config/settings.py
@@ -37,7 +37,10 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['interna.coredump.ch']
+if DEBUG:
+    ALLOWED_HOSTS = []
+else:
+    ALLOWED_HOSTS = ['interna.coredump.ch']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
