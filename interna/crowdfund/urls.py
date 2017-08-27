79aca07a8bb5253229cc1bea50b9a1496899d288
Danilo Bargen <mail@dbrgn.ch>
2017-08-27 23:12:33 +0200

    Add crowdfunding detail page

--- interna/crowdfund/urls.py
+++ interna/crowdfund/urls.py
@@ -6,4 +6,5 @@ app_name = 'crowdfund'
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^projects/(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 ]
--- interna/crowdfund/views.py
+++ interna/crowdfund/views.py
@@ -1,3 +1,4 @@
+from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
 from . import models
@@ -6,3 +7,8 @@ from . import models
 class IndexView(ListView):
     model = models.Project
     template_name = 'crowdfund/list.html'
+
+
+class DetailView(DetailView):
+    model = models.Project
+    template_name = 'crowdfund/detail.html'
