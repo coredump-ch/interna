fd1f3eadeda95338356a9040ea67544f41849cb2
Danilo Bargen <mail@dbrgn.ch>
2017-11-10 00:42:30 +0100

    Remove coupons tab
    It was mostly unused.

--- interna/front/urls.py
+++ interna/front/urls.py
@@ -14,7 +14,6 @@ urlpatterns = [
     url(r'^members/emails/$', views.MemberEmailsView.as_view(), name='member_emails'),
     url(r'^projects/$', TemplateView.as_view(template_name='front/projects.html'),
         name='projects'),
-    url(r'^coupons/$', views.CouponsView.as_view(), name='coupons'),
     url(r'^wishlist/$', TemplateView.as_view(template_name='front/wishlist.html'),
         name='wishlist'),
 ]
