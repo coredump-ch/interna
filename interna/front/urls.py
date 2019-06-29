bb78c60a2884ef098d4198c94bad79b8812b6f02
Danilo Bargen <mail@dbrgn.ch>
2019-06-29 16:12:16 +0200

    Upgrade to Django 2.1

--- interna/front/urls.py
+++ interna/front/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.contrib.auth.views import login
+from django.contrib.auth.views import LoginView
 from django.views.generic.base import TemplateView
 
 from . import views
@@ -8,7 +8,7 @@ app_name = 'front'
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
-    path('auth/login/', login, {'template_name': 'front/login.html'}, name='login'),
+    path('auth/login/', LoginView.as_view(template_name='front/login.html'), name='login'),
     path('auth/logout/', views.LogoutView.as_view(), name='logout'),
     path('members/', views.MembersView.as_view(), name='members'),
     path('members/emails/', views.MemberEmailsView.as_view(), name='member_emails'),
