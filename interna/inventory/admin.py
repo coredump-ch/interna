21cc5f89e7db93c049d0c11117faa179efa96355
Danilo Bargen <mail@dbrgn.ch>
2019-07-06 15:06:26 +0200

    Inventory: Allow saving as copy in admin

--- interna/inventory/admin.py
+++ interna/inventory/admin.py
@@ -6,6 +6,7 @@ from . import models
 class ItemAdmin(admin.ModelAdmin):
     list_display = ('name', 'identifier', 'owner', 'since')
     list_filter = ('category', 'owner', 'manager')
+    save_as = True
 
 
 admin.site.register(models.Item, ItemAdmin)
