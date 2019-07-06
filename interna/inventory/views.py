e7d2681806623c40075bd1846f9a10bc3e30d77d
Danilo Bargen <mail@dbrgn.ch>
2019-07-06 19:10:28 +0200

    Inventory: Implement category filtering

--- interna/inventory/views.py
+++ interna/inventory/views.py
@@ -8,6 +8,26 @@ class Index(ListView):
     model = models.Item
     template_name = 'inventory/list.html'
 
+    def get_queryset(self):
+        category = self.request.GET.get('category')
+        if category == 'none':
+            return self.model.objects.filter(category__isnull=True)
+        if category is not None and category in models.Item.CATEGORY:
+            return self.model.objects.filter(category=category)
+        return self.model.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        category = self.request.GET.get('category')
+        if category is not None and category in models.Item.CATEGORY:
+            count = self.model.objects.filter(category=category).count()
+            context['category'] = (category, models.Item.CATEGORY[category], count)
+        context['categories'] = [
+            c + (self.model.objects.filter(category=c[0]).count(),)
+            for c in models.Item.CATEGORY
+        ]
+        return context
+
 
 class Detail(DetailView):
     model = models.Item
