404f056a64fad6c7ba9cf0f6b157dd43f76d7a01
Danilo Bargen <mail@dbrgn.ch>
2019-07-08 22:54:23 +0200

    Inventory: Generate P-Touch labels (#53)
    Also, add an edit button to detail page for logged in staff members.

--- interna/inventory/urls.py
+++ interna/inventory/urls.py
@@ -7,4 +7,5 @@ app_name = 'inventory'
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
     path('items/<str:pk>/', views.Detail.as_view(), name='detail'),
+    path('items/<str:pk>/label/', views.Label.as_view(), name='label'),
 ]
