dceea4de0fb530f825b3584cb9bb2c3dffcd641b
Danilo Bargen <mail@dbrgn.ch>
2023-11-07 23:59:34 +0100

    Upgrade Django: 3.1 → 3.2 LTS

--- interna/config/settings.py
+++ interna/config/settings.py
@@ -38,6 +38,8 @@ DATABASES = {
     'default': dj_database_url.config(),
 }
 
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 if DEBUG:
