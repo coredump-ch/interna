f244a355fdbe9d3663fa2fd2184ee435c4f91a50
Danilo Bargen <mail@dbrgn.ch>
2025-01-22 14:07:18 +0100

    Add access info to member database

--- interna/memberdb/models.py
+++ interna/memberdb/models.py
@@ -7,6 +7,10 @@ from . import managers
 
 class Member(models.Model):
     """A member of the association."""
+    class SafeAccessType(models.TextChoices):
+        APP = 'app', 'App'
+        CODE = 'code', 'Code'
+
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
@@ -15,6 +19,11 @@ class Member(models.Model):
         help_text='Wohnort')
     github = models.CharField(max_length=32, blank=True,
         help_text='Github Benutzername')
+    key_number = models.CharField(max_length=100, blank=True,
+        help_text='Schlüsselnummer')
+    key_safe = models.CharField(max_length=100, blank=True,
+        choices=SafeAccessType.choices,
+        help_text='Art des Schlüsselsafe-Zugriffs')
 
     class Meta:
         ordering = ('name', 'id')
